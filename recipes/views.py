45afe8ded1904544782c0fdb916001b59ce6ff38
Minko <mf0981115811@gmail.com>
2024-05-31 10:05:43 +0300

    can see main html

--- recipes/views.py
+++ recipes/views.py
@@ -4,8 +4,8 @@ from django.db.models import Count
 
 def main(request):
     latest_recipes = Recipe.objects.order_by('-id')[:5]
-    return render(request, 'recipe/main.html', {'latest_recipes': latest_recipes})
+    return render(request, 'main.html', {'latest_recipes': latest_recipes})
 
 def category_list(request):
     categories = Category.objects.annotate(recipe_count=Count('recipes'))
-    return render(request, 'recipe/category_list.html', {'categories': categories})
+    return render(request, 'category_list.html', {'categories': categories})
