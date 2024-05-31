5ffecae507d3bdd8cbc6a9c59f481c9962f13f26
Minko <mf0981115811@gmail.com>
2024-05-31 10:11:36 +0300

    test setup

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Recipe, Category
 
-# Create your tests here.
+class RecipeViewsTestCase(TestCase):
+    def setUp(self):
+        Category.objects.create(name='Category 1')
+        Category.objects.create(name='Category 2')
+
+        Recipe.objects.create(name='Recipe 1', category='Category 1')
+        Recipe.objects.create(name='Recipe 2', category='Category 2')
+        Recipe.objects.create(name='Recipe 3', category='Category 1')
