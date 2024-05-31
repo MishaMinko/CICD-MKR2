da1fbf523d7935de02887fe07e64ec5bc4f22d23
Minko <mf0981115811@gmail.com>
2024-05-31 10:14:57 +0300

    fixed setup main test

--- recipes/tests.py
+++ recipes/tests.py
@@ -4,9 +4,14 @@ from .models import Recipe, Category
 
 class RecipeViewsTestCase(TestCase):
     def setUp(self):
-        Category.objects.create(name='Category 1')
-        Category.objects.create(name='Category 2')
+        category_1 = Category.objects.create(name='Category 1')
+        category_2 = Category.objects.create(name='Category 2')
 
-        Recipe.objects.create(name='Recipe 1', category='Category 1')
-        Recipe.objects.create(name='Recipe 2', category='Category 2')
-        Recipe.objects.create(name='Recipe 3', category='Category 1')
+        Recipe.objects.create(name='Recipe 1', category=category_1)
+        Recipe.objects.create(name='Recipe 2', category=category_2)
+        Recipe.objects.create(name='Recipe 3', category=category_1)
+
+    def test_main_view(self):
+            response = self.client.get(reverse('main'))
+            self.assertEqual(response.status_code, 200)
+            self.assertQuerysetEqual(response.context['latest_recipes'], ['<Recipe: Recipe 3>', '<Recipe: Recipe 2>', '<Recipe: Recipe 1>'])
